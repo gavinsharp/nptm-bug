da132b9ac165d197b740f5403c745078bd69a62a
Gavin Sharp <gavin@gavinsharp.com>
2019-02-09 10:54:28 -0500

    initial commit

---
+++ next.config.js
@@ -0,0 +1,15 @@
+const withPlugins = require('next-compose-plugins');
+
+const withSass = require('@zeit/next-sass');
+const withTM = require('next-plugin-transpile-modules');
+
+module.exports = withPlugins([
+  withTM({
+    transpileModules: ['empty'],
+  }),
+  withSass,
+], {
+  webpack: (config, { dev }) => {
+    throw 1;
+  },
+});
